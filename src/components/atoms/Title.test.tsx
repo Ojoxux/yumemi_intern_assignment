102f5d375b9bf4cc2f8c8b31e475dde38886b16f
Jou Okuyama <j.okuyama.engineering@gmail.com>
2024-07-05 19:57:28 +0900

    test: mock CSS imports in component tests

--- src/components/atoms/Title.test.tsx
+++ src/components/atoms/Title.test.tsx
@@ -1,6 +1,8 @@
-// src/components/atoms/Title.test.tsx
 import { render, screen } from '@testing-library/react';
 import Title from './Title';
+
+// CSSのインポートをモック化
+jest.mock('../../styles/Header.css', () => ({}));
 
 describe('Title', () => {
   it('renders the title text', () => {
