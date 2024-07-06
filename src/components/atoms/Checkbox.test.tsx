26328dccbe013fe72727341475a16957d3a8109a
Jou Okuyama <j.okuyama.engineering@gmail.com>
2024-07-06 22:00:37 +0900

    test(atoms): add tests for Checkbox component

---
+++ src/components/atoms/Checkbox.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Checkbox from './Checkbox';
+
+describe('Checkbox', () => {
+  it('renders correctly', () => {
+    const { getByRole } = render(<Checkbox id="test" checked={false} onChange={() => {}} />);
+    expect(getByRole('checkbox')).toBeInTheDocument();
+  });
+
+  it('calls onChange when clicked', () => {
+    const mockOnChange = jest.fn();
+    const { getByRole } = render(<Checkbox id="test" checked={false} onChange={mockOnChange} />);
+    fireEvent.click(getByRole('checkbox'));
+    expect(mockOnChange).toHaveBeenCalledWith(true);
+  });
+});
