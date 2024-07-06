34ab026c881fa9935db4a45365011c36b03630d4
Jou Okuyama <j.okuyama.engineering@gmail.com>
2024-07-07 07:57:39 +0900

    feat(atoms): add Button component and use it in HomePage

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import BasicTemplate from '../../components/templates/BasicTemplate';
 import PrefectureSelector from '../../components/organisms/PrefectureSelector';
 import PopulationGraph from '../../components/organisms/PopulationGraph';
+import Button from '../../components/atoms/Button';
 import { usePrefectureData } from '../../hooks/usePrefectureData';
+import styles from './HomePage.module.css';
 
 const HomePage: React.FC = () => {
   const { prefectures, selectedPrefectures, populationData, handlePrefectureChange, clearAllSelections } = usePrefectureData();
@@ -13,8 +15,10 @@ const HomePage: React.FC = () => {
         prefectures={prefectures}
         selectedPrefectures={selectedPrefectures}
         onPrefectureChange={handlePrefectureChange}
-        onClearAll={clearAllSelections}
       />
+      <div className={styles.clearButtonContainer}>
+        <Button onClick={clearAllSelections}>全て外す</Button>
+      </div>
       <PopulationGraph data={populationData} />
     </BasicTemplate>
   );
