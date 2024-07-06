ce9efce2abe9bd0982ad7e1502f9acb81e2a9a7e
Jou Okuyama <ojoxux@gmail.com>
2024-07-06 23:07:49 +0900

    refac(molecules): update CheckboxWithLabel to use CSS module for styling

--- src/components/molecules/CheckboxWithLabel.tsx
+++ src/components/molecules/CheckboxWithLabel.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Checkbox from '../atoms/Checkbox';
 import Label from '../atoms/Label';
+import styles from './CheckboxWithLabel.module.css';
 
 interface CheckboxWithLabelProps {
   label: string;
@@ -9,7 +10,7 @@ interface CheckboxWithLabelProps {
 }
 
 const CheckboxWithLabel: React.FC<CheckboxWithLabelProps> = ({ label, checked, onChange }) => (
-  <div className="checkbox-with-label">
+  <div className={styles.checkboxWithLabel}>
     <Checkbox checked={checked} onChange={onChange} id={`checkbox-${label}`} />
     <Label htmlFor={`checkbox-${label}`}>{label}</Label>
   </div>
