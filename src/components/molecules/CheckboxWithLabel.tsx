7106d6a8b11309d37c3ffa13eb0c578349b71596
Jou Okuyama <j.okuyama.engineering@gmail.com>
2024-07-06 18:22:54 +0900

    feat(molecules): add CheckboxWithLabel molecule component

--- src/components/molecules/CheckboxWithLabel.tsx
+++ src/components/molecules/CheckboxWithLabel.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Checkbox from '../atoms/Checkbox';
+import Label from '../atoms/Label';
 
 interface CheckboxWithLabelProps {
   label: string;
@@ -6,13 +8,11 @@ interface CheckboxWithLabelProps {
   onChange: (checked: boolean) => void;
 }
 
-const CheckboxWithLabel: React.FC<CheckboxWithLabelProps> = ({ label, checked, onChange }) => {
-  return (
-    <label>
-      <input type="checkbox" checked={checked} onChange={(e) => onChange(e.target.checked)} />
-      {label}
-    </label>
-  );
-};
+const CheckboxWithLabel: React.FC<CheckboxWithLabelProps> = ({ label, checked, onChange }) => (
+  <div className="checkbox-with-label">
+    <Checkbox checked={checked} onChange={onChange} id={`checkbox-${label}`} />
+    <Label htmlFor={`checkbox-${label}`}>{label}</Label>
+  </div>
+);
 
 export default CheckboxWithLabel;
