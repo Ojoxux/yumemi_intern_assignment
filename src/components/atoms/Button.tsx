4ef7f0d01c7764c196cb540bf1ccf688e3adb67c
Jou Okuyama <ojoxux@gmail.com>
2024-07-07 07:57:39 +0900

    feat(atoms): add Button component and use it in HomePage

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Button.module.css';
+
+interface ButtonProps {
+  onClick: () => void;
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ onClick, children, className }) => (
+  <button onClick={onClick} className={`${styles.button} ${className}`}>
+    {children}
+  </button>
+);
+
+export default Button;
