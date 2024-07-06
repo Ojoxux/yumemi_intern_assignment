b3669b334835870fd9420c16be270c645670885d
Jou Okuyama <j.okuyama.engineering@gmail.com>
2024-07-07 01:57:36 +0900

    refac: update import path for usePrefectureData in HomePage.tsx

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import BasicTemplate from '../../components/templates/BasicTemplate';
 import PrefectureSelector from '../../components/organisms/PrefectureSelector';
 import PopulationGraph from '../../components/organisms/PopulationGraph';
-import { usePrefectureData } from '../hooks/usePrefectureData';
+import { usePrefectureData } from '../../hooks/usePrefectureData';
 
 const HomePage: React.FC = () => {
   const { prefectures, selectedPrefectures, populationData, handlePrefectureChange } = usePrefectureData();
