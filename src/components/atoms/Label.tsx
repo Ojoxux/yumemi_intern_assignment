e3c975da0935a332e7eebd2d45c71dd27f8d4d44
Jou Okuyama <ojoxux@gmail.com>
2024-07-06 18:04:06 +0900

    feat(atoms): add Label component

---
+++ src/components/atoms/Label.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface LabelProps {
+  htmlFor: string;
+  children: React.ReactNode;
+}
+
+const Label: React.FC<LabelProps> = ({ htmlFor, children }) => (
+  <label htmlFor={htmlFor}>{children}</label>
+);
+
+export default Label;
