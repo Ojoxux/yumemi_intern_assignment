6280a88a669d09d6971544e9c8c928ad5e442de1
Jou Okuyama <ojoxux@gmail.com>
2024-07-07 17:06:04 +0900

    test: update setupTests to set test API key

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,6 @@
 import ResizeObserver from 'resize-observer-polyfill';
 import '@testing-library/jest-dom';
+process.env.VITE_REACT_APP_RESAS_API = 'test-api-key';
 
 if (typeof window !== 'undefined') {
   window.ResizeObserver = ResizeObserver;
