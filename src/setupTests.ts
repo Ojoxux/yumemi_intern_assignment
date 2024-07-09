966dc24d9b8f20f6bf0b0a1ab69262d45996df87
Jou Okuyama <ojoxux@gmail.com>
2024-07-09 20:05:54 +0900

    refac: replace ResizeObserver polyfill with mock implementation

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,9 +1,13 @@
-import ResizeObserver from 'resize-observer-polyfill';
 import '@testing-library/jest-dom';
 
-if (typeof window !== 'undefined') {
-  window.ResizeObserver = ResizeObserver;
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
 }
+
+window.ResizeObserver = ResizeObserver;
+
 if (typeof process.env.VITE_REACT_APP_RESAS_API_KEY === 'undefined') {
   process.env.VITE_REACT_APP_RESAS_API_KEY = 'test-api-key';
 }
