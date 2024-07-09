c2978b68160fd7a6bce605627aaf06075a158989
Jou Okuyama <ojoxux@gmail.com>
2024-07-10 02:33:20 +0900

    chore: remove unused React import from PrefectureSelector test

--- src/components/organisms/PrefectureSelector.test..tsx
+++ src/components/organisms/PrefectureSelector.test..tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import PrefectureSelector from './PrefectureSelector';
 import { Prefecture } from '../../types';
