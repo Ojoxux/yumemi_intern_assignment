b2b8f15a6450e4e96c3143425dc1ed43afa4bf8f
Jou Okuyama <j.okuyama.engineering@gmail.com>
2024-07-10 03:09:46 +0900

    refac: move PrefectureSelector component files to PrefectureSelector folder

--- src/components/organisms/PrefectureSelector/PrefectureSelector.test..tsx
+++ src/components/organisms/PrefectureSelector/PrefectureSelector.test..tsx
@@ -1,6 +1,6 @@
 import { render, fireEvent, screen } from '@testing-library/react';
 import PrefectureSelector from './PrefectureSelector';
-import { Prefecture } from '../../types';
+import { Prefecture } from '../../../types';
 
 const mockPrefectures: Prefecture[] = [
   { prefCode: 1, prefName: '北海道' },
